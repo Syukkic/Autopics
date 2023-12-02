570f1bc659cfdaa55c1d8058efb44016ba5ddb97
Yuki.N <Syukkic@Gmail.com>
2023-12-02 15:04:24 +0800

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="flex p-5 justify-between sticky top-0 bg-white z-50 shadow-md">
+      <div className="flex space-x-2 items-center">
+        <Image src="/open-ai-logo.png" alt="logo" width={30} height={30} />
+      </div>
+      <div>
+        <h1 className="font-bold">AI Arts Generation</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
