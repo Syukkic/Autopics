fe8242c3c97256e0eefd9a78dd67b2fdfb4a8de3
Yuki.N <Syukkic@Gmail.com>
2023-12-04 20:29:24 +0800

    Handle response image URL

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,12 +1,23 @@
 'use client';
 
+import Image from 'next/image';
 import React, { useState } from 'react';
 
 function PromptInput() {
   const [prompt, setPrompt] = useState('');
+  const [imageURL, setImageURL] = useState('');
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setPrompt(e.target.value);
+  };
+
+  type GPTResponse = {
+    created: number;
+    data: {
+      b64_json: null | string;
+      revised_prompt: string;
+      url: string;
+    }[];
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -25,26 +36,29 @@ function PromptInput() {
       if (!response.ok) {
         throw new Error('Failed to submit data');
       }
-      const data = await response.json();
-      console.log(`API response ${JSON.stringify(data)}`);
+      const data: GPTResponse = await response.json();
+      setImageURL(data.data[0].url);
       setPrompt('');
     } catch (error) {
       console.error(`Error Submitting data: ${error}`);
     }
   };
   return (
-    <div className="m-10">
-      <form onSubmit={handleSubmit} className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
-        <textarea value={prompt} onChange={handleInputChange} placeholder="Enter your prompt" className="flex-1 p-4 outline-none rounded-md " />
-        <button
-          type="submit"
-          className={`p-4 font-bold ${prompt ? 'bg-green-700 text-white transition-colors duration-200' : 'text-gray-300 cursor-not-allowed'}`}
-          disabled={!prompt}
-        >
-          Generate
-        </button>
-      </form>
-    </div>
+    <>
+      <Image src={imageURL} alt="" width={480} height={480} />
+      <div className="m-10">
+        <form onSubmit={handleSubmit} className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
+          <textarea value={prompt} onChange={handleInputChange} placeholder="Enter your prompt" className="flex-1 p-4 outline-none rounded-md " />
+          <button
+            type="submit"
+            className={`p-4 font-bold ${prompt ? 'bg-green-700 text-white transition-colors duration-200' : 'text-gray-300 cursor-not-allowed'}`}
+            disabled={!prompt}
+          >
+            Generate
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dalleprodsec.blob.core.windows.net',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
